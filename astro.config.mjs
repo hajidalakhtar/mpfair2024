6296c1275dda6b3349a4803999687384728b3f4f
hajidalakhtar <hajidalakhtar@gmail.com>
2024-11-28 15:47:12 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig, passthroughImageService} from 'astro/config';
 
 import react from '@astrojs/react';
 
@@ -6,5 +6,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [react(), tailwind()]
 });
